3d6373fefe2746d2ddcd8a4cbdeff4c619e97716
BasharYounes <bywns3777@gmail.com>
2025-12-15 23:49:29 +0300

    fix problems foreign Keys

---
+++ app/Enums/BookingStatus.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Enums;
+
+enum BookingStatus: string
+{
+    case Upcoming  = 'Upcoming';
+    case Completed = 'Completed';
+    case Cancelled = 'Cancelled';
+
+    /**
+     * القيم التي ستُستخدم في قاعدة البيانات
+     */
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+}
--- database/migrations/2025_12_15_113248_create_doctors_table.php
+++ database/migrations/2025_12_15_113248_create_doctors_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('password');
-            $table->foreignId('specializations_id')->references('id')->on('specializations')->onUpdate('cascade')->onDelete('set null')->nullable();
+            $table->foreignId('specializations_id')->references('id')->on('specializations')->onUpdate('cascade')->onDelete('cascade');
             $table->Integer('mobile_number');
             $table->string('license_number');
             $table->float('session_price');
