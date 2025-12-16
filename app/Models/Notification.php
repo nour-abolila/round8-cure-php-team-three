24c297c9134a1b4f2b7a14bdbe84d6f67d5cd20e
ahmedsh100 <ahmedmohamedsh100@gmail.com>
2025-12-16 23:58:26 +0200

    Update Notification model

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -8,8 +8,21 @@ class Notification extends Model
 {
     protected $fillable = ['title','body','user_id','is_read'];
 
+      protected $casts = [
+        'is_read' => 'boolean'
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function markAsRead()
+    {
+        if (!$this->is_read) {
+            $this->update([
+                'is_read' => true
+            ]);
+        }
     }
 }
