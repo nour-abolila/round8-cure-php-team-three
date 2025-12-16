c972f1b04b6d6ab6c74a7ca37d5e122bce7c6c3c
ahmedsh100 <ahmedmohamedsh100@gmail.com>
2025-12-17 00:10:47 +0200

    Add NotificationResource

---
+++ app/Http/Resources/NotificationResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NotificationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body' => $this->body,
+            'is_read' => $this->is_read,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'created_at_human' => $this->created_at->diffForHumans()
+        ];
+    }
+}
