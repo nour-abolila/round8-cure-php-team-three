2cfc2ccdc2ca4c86baf57de5634e9b7310652c98
ahmedsh100 <ahmedmohamedsh100@gmail.com>
2025-12-17 16:40:07 +0200

    Update Doctor Model

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -1,15 +1,53 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
- protected $guarded = [];
+ use HasFactory;
+ protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'specializations_id',
+        'mobile_number',
+        'license_number',
+        'session_price',
+        'availability_slots',
+        'clinic_location'
+    ];
 
- public function bookings()
- {
-    return $this->hasMany(Booking::class);
- }
+    protected $casts = [
+        'session_price' => 'decimal:2',
+        'availability_slots' => 'array',
+        'clinic_location' => 'array'
+    ];
+
+    public function bookings()
+    {
+       return $this->hasMany(Booking::class);
+    }
+
+
+     public function reviews()
+    {
+        return $this->hasMany(Review::class, 'doctor_id');
+    }
+
+    public function specialization()
+    {
+        return $this->belongsTo(Specialization::class, 'specializations_id');
+    }
+
+    public function averageRating()
+    {
+        return $this->reviews()->avg('rating');
+    }
+
+    public function reviewsCount()
+    {
+        return $this->reviews()->count();
+    }
 }
